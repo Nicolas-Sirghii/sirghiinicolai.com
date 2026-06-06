1ca03f0e6bd3835815ed77a15774b81bde70ee0d
Nicolas <nicolas.mailbox100@gmail.com>
2026-06-06 22:31:25 +0300

    red

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 function App() {
  return (
   <div className="App">
-   <h1>Welcome to React Nicolas</h1>
+   <h1 className="textColor">Welcome to React Nicolas</h1>
   </div>
  )
 }
